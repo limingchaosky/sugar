bd6d9fa06f636e04be774ea08d7704de1e907e63
limingchao <limingchaosky@163.com>
2017-12-10 20:36:52 +0800

    add interface sugarAndSugarfull

--- src/main/java/org/TeamS/sugar/service/impl/SugarService.java
+++ src/main/java/org/TeamS/sugar/service/impl/SugarService.java
@@ -1,11 +1,12 @@
 package org.TeamS.sugar.service.impl;
 
-import org.TeamS.sugar.entity.Sugar;
+import org.TeamS.sugar.entity.SugarFull;
 import org.TeamS.sugar.mapper.SugarFullMapper;
 import org.TeamS.sugar.service.ISugarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,8 +16,14 @@ public class SugarService implements ISugarService {
     private SugarFullMapper sugarFullMapper;
 
     @Override
-    public List<Sugar> getSugarListByUserId(int userId) {
-        List<Sugar> sugarList = sugarFullMapper.getListByUserId(userId);
+    public List<SugarFull> getSugarListByUserId(int userId) {
+        List<SugarFull> sugarList = sugarFullMapper.getListByUserId(userId);
+        return sugarList;
+    }
+
+    @Override
+    public List<SugarFull> getSugarListByUserIdAndDate(int userId, Date startDate, Date endDate) {
+        List<SugarFull> sugarList = sugarFullMapper.getListByUserIdAndSugarDate(userId, startDate, endDate);
         return sugarList;
     }
 }
